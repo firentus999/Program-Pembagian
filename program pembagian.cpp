8ae7095603b466e71f791a3a55e1b32acfa067aa
Firentus Agustone Pasaribu <45529731+firentus999@users.noreply.github.com>
2018-12-12 09:25:53 +0700

    Program

---
+++ program pembagian.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+const char nama[]="firentus agustone";
+int main()
+{
+    printf (nama);
+    int x,y;
+    float hasil;
+    cout<<" masukan bilangan:";
+    cin>>x;
+    cout<<"masukan bilangan:";
+    cin>>y;
+    hasil=x/y;
+    cout<<"hasil pembagian adalah"<<hasil;
+    return 0;
+}
